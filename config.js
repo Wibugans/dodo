e546d54fb2b617c957a2ef7832dc17a5d492911f
Wibugans <95990403+Wibugans@users.noreply.github.com>
2022-03-09 11:50:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 let fs = require('fs')
-global.owner = ['919539102851'] // Letakan nomor kamu disini
+global.owner = ['6281275274328'] // Letakan nomor kamu disini
 global.mods = [] // Moderator?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Pengguna premium tidak memerlukan limit
 global.APIs = { // API Prefix
@@ -31,14 +31,14 @@ global.APIKeys = { // APIKey nya disini
 }
 
 // Sticker WM
-global.packname = 'Eva-Sticker'
-global.author = 'Eva'
+global.packname = 'Yusril-Ganteng'
+global.author = 'Yusril'
 
 global.wait = '_*Please Wait..*_'
 global.eror = '_*Server Error*_'
 global.fla = 'https://ibb.co/jW31z7n'
 
-global.watermark = '© EVA'
+global.watermark = '© Yusril'
 
 global.multiplier = 69 // Semakin tinggi, semakin sulit naik level
 
